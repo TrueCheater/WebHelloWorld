11deb2dba3f266e024091b5998103c5b6615ec13
kate <kate.khomra@gmail.com>
2022-02-24 01:24:06 +0200

    lab 1 WebHelloWorld

---
+++ src/main/java/com/example/webhelloworld/WebHelloWorldApplication.java
@@ -0,0 +1,12 @@
+package com.example.webhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebHelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebHelloWorldApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/webhelloworld/WebHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webhelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebHelloWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
